4197ba2ac48aa17c2dcc0a03e534f6b3d4184270
Arnontid <615021000620@mail.rmutk.ac.th>
2021-02-24 09:49:28 +0700

    Lap

---
+++ app/views/partials/post/add.php
@@ -0,0 +1,194 @@
+
+<?php
+$comp_model = new SharedController;
+
+$csrf_token = Csrf :: $token;
+
+$show_header = $this->show_header;
+$view_title = $this->view_title;
+$redirect_to = $this->redirect_to;
+
+?>
+
+<section class="page">
+    
+    <?php
+    if( $show_header == true ){
+    ?>
+    
+    <div  class="bg-light p-3 mb-3">
+        <div class="container">
+            
+            <div class="row ">
+                
+                <div class="col-12 comp-grid">
+                    <h3 class="record-title">Add New Post</h3>
+                    
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    
+    <?php
+    }
+    ?>
+    
+    <div  class="">
+        <div class="container">
+            
+            <div class="row ">
+                
+                <div class="col-md-7 comp-grid">
+                    
+                    <?php $this :: display_page_errors(); ?>
+                    
+                    <div  class=" animated fadeIn">
+                        <form id="post-add-form" role="form" novalidate enctype="multipart/form-data" class="form form-horizontal needs-validation" action="<?php print_link("post/add?csrf_token=$csrf_token") ?>" method="post">
+                            <div>
+                                
+                                
+                                <div class="form-group ">
+                                    <div class="row">
+                                        <div class="col-sm-4">
+                                            <label class="control-label" for="headline">Headline <span class="text-danger">*</span></label>
+                                        </div>
+                                        <div class="col-sm-8">
+                                            <div class="">
+                                                <input id="ctrl-headline"  value="<?php  echo $this->set_field_value('headline',''); ?>" type="text" placeholder="Enter Headline"  required="" name="headline"  class="form-control " />
+                                                    
+                                                    
+                                                    
+                                                </div>
+                                                
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+                                    
+                                    
+                                    
+                                    <div class="form-group ">
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <label class="control-label" for="title">Title <span class="text-danger">*</span></label>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <div class="">
+                                                    <input id="ctrl-title"  value="<?php  echo $this->set_field_value('title',''); ?>" type="text" placeholder="Enter Title"  required="" name="title"  class="form-control " />
+                                                        
+                                                        
+                                                        
+                                                    </div>
+                                                    
+                                                    
+                                                </div>
+                                            </div>
+                                        </div>
+                                        
+                                        
+                                        
+                                        
+                                        <div class="form-group ">
+                                            <div class="row">
+                                                <div class="col-sm-4">
+                                                    <label class="control-label" for="category">Category <span class="text-danger">*</span></label>
+                                                </div>
+                                                <div class="col-sm-8">
+                                                    <div class="">
+                                                        <input id="ctrl-category"  value="<?php  echo $this->set_field_value('category',''); ?>" type="text" placeholder="Enter Category"  required="" name="category"  class="form-control " />
+                                                            
+                                                            
+                                                            
+                                                        </div>
+                                                        
+                                                        
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            
+                                            
+                                            
+                                            
+                                            <div class="form-group ">
+                                                <div class="row">
+                                                    <div class="col-sm-4">
+                                                        <label class="control-label" for="content">Content <span class="text-danger">*</span></label>
+                                                    </div>
+                                                    <div class="col-sm-8">
+                                                        <div class="">
+                                                            <input id="ctrl-content"  value="<?php  echo $this->set_field_value('content',''); ?>" type="text" placeholder="Enter Content"  required="" name="content"  class="form-control " />
+                                                                
+                                                                
+                                                                
+                                                            </div>
+                                                            
+                                                            
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                
+                                                
+                                                
+                                                
+                                                <div class="form-group ">
+                                                    <div class="row">
+                                                        <div class="col-sm-4">
+                                                            <label class="control-label" for="author">Author <span class="text-danger">*</span></label>
+                                                        </div>
+                                                        <div class="col-sm-8">
+                                                            <div class="">
+                                                                <input id="ctrl-author"  value="<?php  echo $this->set_field_value('author',''); ?>" type="text" placeholder="Enter Author"  required="" name="author"  class="form-control " />
+                                                                    
+                                                                    
+                                                                    
+                                                                </div>
+                                                                
+                                                                
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    
+                                                    
+                                                    
+                                                    
+                                                    <div class="form-group ">
+                                                        <div class="row">
+                                                            <div class="col-sm-4">
+                                                                <label class="control-label" for="status">Status <span class="text-danger">*</span></label>
+                                                            </div>
+                                                            <div class="col-sm-8">
+                                                                <div class="">
+                                                                    <input id="ctrl-status"  value="<?php  echo $this->set_field_value('status',''); ?>" type="text" placeholder="Enter Status"  required="" name="status"  class="form-control " />
+                                                                        
+                                                                        
+                                                                        
+                                                                    </div>
+                                                                    
+                                                                    
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        
+                                                        
+                                                        
+                                                        
+                                                    </div>
+                                                    <div class="form-group form-submit-btn-holder text-center">
+                                                        <button class="btn btn-primary" type="submit">
+                                                            
+                                                            <i class="fa fa-send"></i>
+                                                        </button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                            
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </section>
+                        
